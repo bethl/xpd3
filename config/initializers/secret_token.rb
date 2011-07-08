8d9d3dbd74e3824eaf43baba3e993110bc5c3bad
root <root@sweets.localdomain>
2011-07-07 19:34:12 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xpd3::Application.config.secret_token = '7617bdc2001ec9382ae27cc6cd0feb56efb3c0c44b1bdcd7169212a3e2bcb5d1bb0b281492243cb9e524e7233382decc4bedff0d558beced19a6e32090377726'
