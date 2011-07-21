5614ea52b4a4f0c007d7041f21b247b8ab8bc358
excelsio <excelsio@whitesox.accountservergroup.com>
2011-07-21 15:15:56 -0500

    updated news blast for crazy day

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @newsblast = "Crazy Days is just arround the corner!</p><br>  <p>We'll have a booth on Water St. this Thurs/Fri/Sat.</p><br>  <p>We will have in-store specials.  Hope to see you there!"
+    @newsblast = "Crazy Days are on NOW!</p><br>  <p>We currently have a booth on Water St.  It will be there till Sat.</p><br>  <p>We will have in-store specials.  Hope to see you there!"
     # A news blast shall consist of
     # Date
     # Headline
